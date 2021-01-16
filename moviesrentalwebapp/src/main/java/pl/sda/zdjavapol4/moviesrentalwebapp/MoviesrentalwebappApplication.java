d9d0660db19848bb24df95e1b3a2e7ec826f2361
[Błażej Karnecki] <blazej.karnecki@gmail.com>
2021-01-16 12:36:27 +0100

    [add] application.properties to git

---
+++ moviesrentalwebapp/src/main/java/pl/sda/zdjavapol4/moviesrentalwebapp/MoviesrentalwebappApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.zdjavapol4.moviesrentalwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesrentalwebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesrentalwebappApplication.class, args);
+	}
+
+}
